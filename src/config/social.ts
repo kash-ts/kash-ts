e52fd439f6b96257dfa0709004d6e30381efe8f9
Mikhail <ipomainkra@gmail.com>
2026-08-11 20:28:35 +1000

    chore: update gitignore and clean up unused default public assets

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,6 +1,5 @@
 export const SOCIAL_LINKS = {
   github: 'https://github.com/kash-ts',
-  donations: 'https://www.donationalerts.com/r/k-team',
-  services: 'https://www.donationalerts.com/r/k-team',
+  donations: 'https://www.donationalerts.com/r/k_team',
   email: 'ipomainkra@gmail.com',
 } as const;
